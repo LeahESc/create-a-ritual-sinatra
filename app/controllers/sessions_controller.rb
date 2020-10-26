dfa78e77daa9af061acf66e1dfc9ebd11ed55d8b
LeahESc <leahemily1@gmail.com>
2020-10-26 12:04:36 -0700

    "changes redirect on /login to categories

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
         @user = User.find_by(username: params[:username])
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id 
-            redirect "/rituals"
+            redirect "/categories"
         else  
             redirect '/signup'
         end
