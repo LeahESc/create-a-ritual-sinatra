9c382e8bce7c59f8976dca4b830558260ab854b9
LeahESc <leahemily1@gmail.com>
2020-10-26 12:01:05 -0700

    "checks for user login"

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,20 @@
 class CategoriesController < ApplicationController
 
-    get '/categories' do 
-        erb :'categories/index'
+    get '/categories' do
+        if logged_in? 
+            @categories = current_user.categories
+            erb :'categories/index'
+        else  
+            redirect '/login'
+        end 
     end
+
+    get '/categories/:id' do
+        if logged_in? 
+            @category = current_user.categories.find_by(id: params[:id])
+        erb :'categories/show'
+        else  
+            redirect '/login'
+        end
+    end 
 end 
