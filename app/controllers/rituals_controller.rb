56f135b59587e90b46d1af5b448cccd7772d6a45
LeahESc <leahemily1@gmail.com>
2020-10-29 10:12:30 -0700

    uses helper methods in rituals and categories controller

--- app/controllers/rituals_controller.rb
+++ app/controllers/rituals_controller.rb
@@ -1,59 +1,45 @@
 class RitualsController < ApplicationController
 
     get "/rituals" do 
-        if logged_in?
-            @rituals = current_user.rituals
+        redirect?
+        @rituals = current_user.rituals
             # @categories.each do |c|
         erb :'rituals/index'
-        else  
-            redirect '/login'
-        end
     end 
 
     get '/rituals/new' do
-        if logged_in?
-            @category_selected = false
-            @categories = Category.all 
-            erb :'rituals/new'
-        else  
-            redirect '/login'
-        end
+        redirect?
+        @category_selected = false
+        @categories = Category.all 
+        erb :'rituals/new'
     end 
     
     get '/categories/:category_id/rituals/new' do
-        if logged_in?
-            @category_selected = true
-            @category_id = params[:category_id]
-            erb :'rituals/new'
-        else  
-            redirect '/login'
-        end
+        redirect?
+        @category_selected = true
+        @category_id = params[:category_id]
+        erb :'rituals/new'
     end
 
     post '/rituals' do 
-        if logged_in?
-            @ritual = current_user.rituals.build(title: params[:title], description: params[:description], category_id: params[:category_id])
-            if !!params["name"] && !params["name"].empty? 
-                @ritual.category = Category.find_or_create_by(name: params[:name])
-            end
-            if @ritual.save
-                redirect '/categories'
-            else  
-                @message = @ritual.errors.full_messages.join(". ")
-                erb :'rituals/new'
-            end
+        redirect?
+        @ritual = current_user.rituals.build(title: params[:title], description: params[:description], category_id: params[:category_id])
+        if !!params["name"] && !params["name"].empty? 
+            @ritual.category = Category.find_or_create_by(name: params[:name])
+        end
+        if @ritual.save
+            redirect '/categories'
         else  
-            redirect '/login'
+            @message = @ritual.errors.full_messages.join(". ")
+            erb :'rituals/new'
         end 
     end
 
     get '/rituals/:id' do 
-        if logged_in? 
-            set_ritual
-            erb :'rituals/show'
-        else  
-            redirect '/login'
-        end 
+        redirect?
+        set_ritual
+        erb :'rituals/show'
+       
     end
 
     get '/rituals/:id/edit' do
