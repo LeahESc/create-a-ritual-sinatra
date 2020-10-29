818d000fcd9dacd8c06fb7b8ffbbdf17bbb232c9
LeahESc <leahemily1@gmail.com>
2020-10-29 10:12:53 -0700

    add redirect? method in categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,26 +1,20 @@
 class CategoriesController < ApplicationController
 
     get '/categories' do
-        if logged_in? 
-            @categories = current_user.categories.uniq
-            @rituals = current_user.rituals.uniq
-            erb :'categories/index'
-        else  
-            redirect '/login'
-        end 
+        redirect?
+        @categories = current_user.categories.uniq
+        @rituals = current_user.rituals.uniq
+        erb :'categories/index' 
     end
 
     get '/categories/:id' do
-        if logged_in? 
-            @category = current_user.categories.find_by(id: params[:id])
-            if @category
-                @rituals = current_user.rituals.where(category_id: params[:id])
-                erb :'categories/show'
-            else  
-                redirect '/categories'
-            end
+        redirect? 
+        @category = current_user.categories.find_by(id: params[:id])
+        if @category
+            @rituals = current_user.rituals.where(category_id: params[:id])
+            erb :'categories/show'
         else  
-            redirect '/login'
+            redirect '/categories'
         end
     end 
 end 
